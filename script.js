5e3578183ea9fa6a3a868738666e2a912996776f
David Annex <davidmannex@gmail.com>
2023-02-13 16:16:25 -0500

    added color coding and removed extranious code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $("#currentDay").text(moment().format('dddd MMMM Do'));
 
-var currentTime= moment().hours();
+var currentHour= moment().hours();
 
 function load(){
     $("#9am").children(":input").val(localStorage.getItem("9am"));
@@ -12,19 +12,26 @@ function load(){
     $("#3pm").children(":input").val(localStorage.getItem("3pm"));
     $("#4pm").children(":input").val(localStorage.getItem("4pm"));
     $("#5pm").children(":input").val(localStorage.getItem("5pm"));
+
 }
 
-function save(){
-   
-}
+$(".row").each(function(){
+    var entryHour = parseInt($(this).attr("id"));
+    if (entryHour<9)
+        entryHour=entryHour+12; //if its the afternoon add 12 because current hour is in military time
+    if(entryHour>currentHour)// future event
+        $(this).addClass("future");
+    if(entryHour<currentHour)// past event
+        $(this).addClass("past");
+    else
+        $(this).addClass("present");
+        
+})
 
 $(".saveBtn").on("click",function(){
     var thisHour=$(this).parent().attr("id");
     var thisText=$(this).siblings(":input").val();
     localStorage.setItem(thisHour,thisText);
 })
-
-var timestamps=["9am","10am","11am","12pm","1pm","2pm","3pm","4pm","5pm"];
-containerEl=document.querySelector("container")
 
 load()
